91826b3f7cfc45f5e51eecc7828e59ab8a4690fc
Reyes-Antonio21 <antonioreyes@u.boisestate.edu>
2025-03-16 20:22:07 -0600

    Checkpoint

--- Teacher_Code.cu
+++ Teacher_Code.cu
@@ -20,7 +20,7 @@ __global__ void applyBoundaryConditionsGPU(float *h, float *uh, float *vh, int n
   int i = threadIdx.x + blockIdx.x * blockDim.x;
   int j = threadIdx.y + blockIdx.y * blockDim.y;
 
-  if (i >= nx + 3 || j >= ny + 3) // Ensure we are within valid bounds
+  if (i >= nx + 2 || j >= ny + 2) // Ensure we are within valid bounds
   return; 
 
   int id, id_ghost;
