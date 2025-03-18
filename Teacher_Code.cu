ab950177cb5eee0aa19b7f068b01a5b6874622a4
Reyes-Antonio21 <antonioreyes@u.boisestate.edu>
2025-03-17 20:06:36 -0600

    CODE WORKS!!!!!!!!

--- Teacher_Code.cu
+++ Teacher_Code.cu
@@ -298,16 +298,21 @@ __global__ void computeFluxesGPU(float *h, float *uh, float *vh, float *fh, floa
   return;
   
   unsigned int id = ID_2D(i, j, nx);
+
   float g = 9.81f; // Gravitational acceleration
-  
   float h_safe = fmaxf(h[id], EPSILON); // Prevent division by zero
   
   // Compute fluxes safely
   fh[id] = uh[id];
+
   fuh[id] = uh[id] * uh[id] / h_safe + 0.5f * g * h_safe * h_safe;
+
   fvh[id] = uh[id] * vh[id] / h_safe;
+
   gh[id] = vh[id];
+
   guh[id] = uh[id] * vh[id] / h_safe;
+
   gvh[id] = vh[id] * vh[id] / h_safe + 0.5f * g * h_safe * h_safe;
   
 }
