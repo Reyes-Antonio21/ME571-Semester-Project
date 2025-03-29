1c9a5dc2bb216b525fe9f0260beb7cd4fcae9c70
Reyes-Antonio21 <antonioreyes@u.boisestate.edu>
2025-03-29 00:36:23 -0600

    ?

--- Shallow_Water_Equation_Experimental.cu
+++ Shallow_Water_Equation_Experimental.cu
@@ -171,8 +171,8 @@ void generateDrops( int nx, int ny, float x[], float y[], float h[])
 
   // Generate random perturbation coordinates
   // Offset added to restrict drop formation on boundary
-  xx_perturbation = nx / 10;
-  yy_perturbation = ny / 5;
+  xx_perturbation = 8 * nx / 10;
+  yy_perturbation = 8 * ny / 5;
 
   for ( i = 1; i < ny+1; i++ )
     for( j = 1; j < nx+1; j++)
