af341e006a9a11ff888c17134da30fb3e07e54eb
Reyes-Antonio21 <antonioreyes@u.boisestate.edu>
2025-04-22 21:50:38 -0600

    reverted back to weird behavior

--- run_script_performance_mpi_total.bash
+++ run_script_performance_mpi_total.bash
@@ -12,7 +12,28 @@ module load openmpi
 module load slurm
 #source ~/.bashrc
 
-for p in 1 2 4 8 12 16 24 32 48
-do 
-    srun -np $p ./swe_2d_mpi 100 100 0.008 10 10 0.5
-done
+mpirun -np 1 ./swe_2d_mpi
+mpirun -np 2 ./swe_2d_mpi
+mpirun -np 4 ./swe_2d_mpi
+mpirun -np 6 ./swe_2d_mpi 
+mpirun -np 8 ./swe_2d_mpi
+mpirun -np 10 ./swe_2d_mpi 
+mpirun -np 12 ./swe_2d_mpi
+mpirun -np 14 ./swe_2d_mpi
+mpirun -np 16 ./swe_2d_mpi
+mpirun -np 18 ./swe_2d_mpi 
+mpirun -np 20 ./swe_2d_mpi
+mpirun -np 22 ./swe_2d_mpi
+mpirun -np 24 ./swe_2d_mpi 
+mpirun -np 26 ./swe_2d_mpi
+mpirun -np 28 ./swe_2d_mpi 
+mpirun -np 30 ./swe_2d_mpi
+mpirun -np 32 ./swe_2d_mpi
+mpirun -np 34 ./swe_2d_mpi 
+mpirun -np 36 ./swe_2d_mpi
+mpirun -np 38 ./swe_2d_mpi 
+mpirun -np 40 ./swe_2d_mpi
+mpirun -np 42 ./swe_2d_mpi
+mpirun -np 44 ./swe_2d_mpi 
+mpirun -np 46 ./swe_2d_mpi
+mpirun -np 48 ./swe_2d_mpi 
