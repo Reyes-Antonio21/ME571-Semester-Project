6aa551119cd29e990acbb1ee022bada7abe50cff
Antonio Reyes <antonioreyes@u.boisestate.edu>
2025-04-24 04:11:17 -0600

    Updating nx for experiment file

--- run_script_performance_cuda_experiment.bash
+++ run_script_performance_cuda_experiment.bash
@@ -4,12 +4,11 @@
 #SBATCH -c 1
 #SBATCH -N 1
 #SBATCH -p gpu
-#SBATCH -t 00:02:30
+#SBATCH -t 00:01:30
 #SBATCH --gres=gpu:1
-#SBATCH --exclusive
 
 # Fixed simulation constants
-nx=200
+nx=2500
 xlen=10
 t_final=0.5
 g=9.81
