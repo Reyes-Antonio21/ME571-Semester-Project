59cd8d2921ed5a5655b5d0a77d83cf6a569bd62b
Reyes-Antonio21 <antonioreyes@u.boisestate.edu>
2025-04-25 22:43:00 -0600

    added exclusive badge to experiment bash for better comparison of results.

--- run_script_performance_cuda_experiment.bash
+++ run_script_performance_cuda_experiment.bash
@@ -6,6 +6,7 @@
 #SBATCH -p gpu
 #SBATCH -t 00:10:00
 #SBATCH --gres=gpu:1
+#SBATCH --exclusive
 
 # Fixed simulation constants
 nx=2500
@@ -22,6 +23,7 @@ dt=$(echo "$CFL * $dx / $c" | bc -l)
 
 # Run CUDA program
 ./swep_2d_ex $nx $dt $xlen $t_final
+
 ./swep_2d_ex 8000 0.0001 10 0.5
 
 ./swep_2d_ex 200 0.004 10 0.5
