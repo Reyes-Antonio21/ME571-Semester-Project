387934e5cce7d2a50fec61f6daf168707b85502c
Reyes-Antonio21 <antonioreyes@u.boisestate.edu>
2025-04-29 06:06:04 -0600

    ...

--- Shallow_Water_Equations_Experimental.cu
+++ Shallow_Water_Equations_Experimental.cu
@@ -272,7 +272,7 @@ __device__ void haloExchange(float* sh_h, float* sh_uh, float* sh_vh, const floa
 }
 
 
-__global__ void shallowWaterSolver(float *__restrict__ h, float *__restrict__ uh, float *__restrict__ vh, float lambda_x, float lambda_y, int nx, int ny, float dt, float finalRuntime)
+__global__ __launch_bounds__(1024,1) void shallowWaterSolver(float *__restrict__ h, float *__restrict__ uh, float *__restrict__ vh, float lambda_x, float lambda_y, int nx, int ny, float dt, float finalRuntime)
 {
   unsigned int i = blockIdx.y * blockDim.y + threadIdx.y;
   unsigned int j = blockIdx.x * blockDim.x + threadIdx.x;
@@ -503,7 +503,7 @@ int main ( int argc, char *argv[] )
 
   // Define the block and grid sizes
   int dimx = 32;
-  int dimy = 20;
+  int dimy = 32;
   dim3 blockSize(dimx, dimy);
   dim3 gridSize((nx + 2 + blockSize.x - 1) / blockSize.x, (ny + 2 + blockSize.y - 1) / blockSize.y);
 
