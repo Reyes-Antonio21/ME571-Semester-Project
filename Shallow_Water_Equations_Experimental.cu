c7f3061d2cc128563161b264d56b2ba244e1bf7b
Antonio Reyes <antonioreyes@u.boisestate.edu>
2025-05-04 14:59:40 -0600

    ...

--- Shallow_Water_Equations_Experimental.cu
+++ Shallow_Water_Equations_Experimental.cu
@@ -512,7 +512,7 @@ __global__ void shallowWaterSolver(float *__restrict__ h, float *__restrict__ uh
     }
     __syncthreads();
     
-    applyReflectiveBCs(sh_h, sh_uh, sh_vh, local_i, local_j)
+    applyReflectiveBCs(sh_h, sh_uh, sh_vh, local_i, local_j);
     __syncthreads();
 
     writeSharedMemToGlobalMem(sh_h, h, nx, ny, global_i, global_j, local_i, local_j);
