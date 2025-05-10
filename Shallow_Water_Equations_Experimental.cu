fdff4139f479c537ea262d0e6c6402d6d950fcda
Antonio Reyes <antonioreyes@u.boisestate.edu>
2025-05-09 23:02:40 -0600

    ...

--- Shallow_Water_Equations_Experimental.cu
+++ Shallow_Water_Equations_Experimental.cu
@@ -449,6 +449,9 @@ __global__ void shallowWaterSolver(float *__restrict__ h, float *__restrict__ uh
     writeGlobalMemToSharedMem(sh_vh, vh, nx, ny, global_i, global_j, local_i, local_j);
     __syncthreads();
 
+    haloExchange(sh_h, sh_uh, sh_vh, h, uh, vh, nx, ny, global_i, global_j, local_i, local_j);
+    __syncthreads();
+
     // === Compute Fluxes (write only to interior region) ===
     if (local_i > 0 && local_i < blockDim.y && local_j > 0 && local_j < blockDim.x)
     {
@@ -477,9 +480,6 @@ __global__ void shallowWaterSolver(float *__restrict__ h, float *__restrict__ uh
       sh_guh[local_id] = uv * inv_h;
       sh_gvh[local_id] = __fmaf_rn(vh2, inv_h, g_half * h2);
     }
-    __syncthreads();
-
-    haloExchange(sh_h, sh_uh, sh_vh, h, uh, vh, nx, ny, global_i, global_j, local_i, local_j);
     __syncthreads();
 
     // === Compute Updated Values Using Stencil ===
