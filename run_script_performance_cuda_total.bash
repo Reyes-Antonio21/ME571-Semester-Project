49e40ef465fc2df79db05d9210590c93ef9789c8
Antonio Reyes <antonioreyes@u.boisestate.edu>
2025-05-12 04:30:06 -0600

    ...

--- run_script_performance_cuda_total.bash
+++ run_script_performance_cuda_total.bash
@@ -14,16 +14,13 @@ t_final=0.5
 g=9.81
 h_max=1.4
 CFL=0.5
-output_file="Shallow_Water_Equations_Cuda_Total_Runtime_Performance.csv"
+output_file="Shallow_Water_Equations_Cuda_Total_Runtime_Performance_Extra.csv"
 
 # CSV Header
 echo "Problem size,Time steps,Iteration,Elapsed time (s),Host-device transfer time (s),Device-host transfer time (s)" > $output_file
 
 # Loop over problem sizes
-for nx in 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000 \
-          2100 2200 2300 2400 2500 2600 2700 2800 2900 3000 3200 3400 3600 3800 4000 \
-          4200 4400 4600 4800 5000 5200 5400 5600 5800 6000 6200 6400 6600 6800 7000 \
-          7200 7400 7600 7800 8000 9000 10000 11000 12000 13000 14000 15000 16000
+for nx in 400 500 600 700 800
 do
     # Compute dt using CFL condition
     dx=$(echo "$xlen / $nx" | bc -l)
